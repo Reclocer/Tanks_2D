e1c73cd48699dfab7b901be63b826ea672f38922
Reclocer <54275125+Reclocer@users.noreply.github.com>
2020-06-26 19:41:06 +0300

    Add Helpers

---
+++ Assets/Scripts/Helpers/GameAreaHelper.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+
+namespace Corebin.Helpers
+{
+    public static class GameAreaHelper
+    {     
+        public static bool IsInGameplayArea(Transform objectTransform, Bounds objectBounds, Camera camera)
+        {
+            var camHalfHeight = camera.orthographicSize;
+            var camHalfWidth = camHalfHeight * camera.aspect;
+            var camPos = camera.transform.position;
+            var topBound = camPos.y + camHalfHeight;
+            var bottomBound = camPos.y - camHalfHeight;
+            var leftBound = camPos.x - camHalfWidth;
+            var rightBound = camPos.x + camHalfWidth;
+
+            var objectPos = objectTransform.position;
+
+            return (objectPos.x - objectBounds.extents.x < rightBound)
+                && (objectPos.x + objectBounds.extents.x > leftBound)
+                && (objectPos.y - objectBounds.extents.y < topBound)
+                && (objectPos.y + objectBounds.extents.y > bottomBound);            
+        }
+
+        #region RestrictMovement
+
+        /// <summary>
+        /// Restrict lateral object movement  
+        /// </summary>
+        /// <param name="objectPosition">Object pozition for restrict</param>
+        /// <param name="deltaPositionX">Horizontal delta position for new position</param>
+        /// <param name="objectBounds">Object bounds</param>
+        /// <returns>Return true if object within GamePlayArea</returns>
+        public static bool RestrictLateralMovement(ref Vector3 objectPosition, float deltaPositionX, Bounds objectBounds, Camera camera)
+        {
+            //TODO:
+            //Ищем ширину и высоту окна игры. 
+            //При каждом вызове этого метода нецелесообразно выполнять поиск параметров окна.
+            //Необходимо выполнять это событийно и в другом классе. 
+            //А сюда передавать screenBounds параметром. 
+            Vector2 screenBounds = camera.ScreenToWorldPoint(new Vector3(Screen.width,
+                                                                         Screen.height,
+                                                                         camera.transform.position.z));
+
+            float pos = objectPosition.x + deltaPositionX;
+            float rightBound = screenBounds.x - objectBounds.size.x / 2;
+            float leftBound = -screenBounds.x + objectBounds.size.x / 2;
+            float positionX = Mathf.Clamp(pos, leftBound, rightBound);
+
+            if (pos > positionX)
+            {
+                objectPosition = new Vector3(rightBound, objectPosition.y, objectPosition.z);
+                return false;
+            }
+            else if (pos < positionX)
+            {
+                objectPosition = new Vector3(leftBound, objectPosition.y, objectPosition.z);
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Restrict longitudinal object movement  
+        /// </summary>
+        /// <param name="objectPosition">Object pozition for restrict</param>
+        /// <param name="deltaPositionY">Vertical delta position for new position</param>
+        /// <param name="objectBounds">Object bounds</param>
+        /// <returns>Return true if object within GamePlayArea</returns>
+        public static bool RestrictLongitudinalMovement(ref Vector3 objectPosition, float deltaPositionY, Bounds objectBounds, Camera camera)
+        {
+            //TODO:
+            //Ищем ширину и высоту окна игры. 
+            //При каждом вызове этого метода нецелесообразно выполнять поиск параметров окна.
+            //Необходимо выполнять это событийно и в другом классе. 
+            //А сюда передавать screenBounds параметром. 
+            Vector2 screenBounds = camera.ScreenToWorldPoint(new Vector3(Screen.width,
+                                                                         Screen.height,
+                                                                         camera.transform.position.z));
+
+            float pos = objectPosition.y + deltaPositionY;
+            float topBound = screenBounds.y - objectBounds.size.y / 2;
+            float bottomBound = -screenBounds.y + objectBounds.size.y / 2;
+            float positionY = Mathf.Clamp(pos, bottomBound, topBound);
+
+            if (pos > positionY)
+            {
+                objectPosition = new Vector3(objectPosition.x, topBound, objectPosition.z);
+                return false;
+            }
+            else if (pos < positionY)
+            {
+                objectPosition = new Vector3(objectPosition.x, bottomBound, objectPosition.z);
+                return false;
+            }
+
+            return true;
+        }
+
+        #endregion RestrictMovement
+
+    }
+}
---
+++ Assets/Scripts/Helpers/OutOfBorderDestructor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Corebin.Helpers
+{
+    public class OutOfBorderDestructor : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer _representation;
+
+        void Update()
+        {
+            CheckBorders();
+        }
+
+        private void CheckBorders()
+        {
+            if (!GameAreaHelper.IsInGameplayArea(transform, _representation.bounds, Camera.main))
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
