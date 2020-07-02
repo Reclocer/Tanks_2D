cc570050f184f4a6472ce4393855b01a201965e6
Reclocer <54275125+Reclocer@users.noreply.github.com>
2020-07-03 02:18:39 +0300

    Add BonusSpawner.cs

---
+++ Assets/Scripts/Bonuses/BonusSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Corebin.Tanks;
+using Corebin.Tanks.Tanks;
+
+namespace Corebin.Tanks.Bonuses
+{
+    [RequireComponent(typeof(GameUnit))]
+    public class BonusSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject _objectForSpawn;
+
+        private GameUnit _unit;
+
+        private void Start()
+        {
+            _unit = GetComponent<TankBase>();
+            _unit.UnitDestroyed += SpawnBonus;
+        }
+
+        private void SpawnBonus()
+        {
+            Instantiate(_objectForSpawn, transform.position, transform.rotation);
+        }
+    }
+}
